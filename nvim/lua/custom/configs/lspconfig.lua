2679e6a3569599e72a2ab3f6cc134445f564bcc7
radekaadek <radekaadek@gmail.com>
2024-09-05 14:08:45 +0200

    u

--- nvim/lua/custom/configs/lspconfig.lua
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -17,7 +17,7 @@ lspconfig.basedpyright.setup {
     },
   },
 }
-lspconfig.tsserver.setup {
+lspconfig.ts_ls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   init_options = {
--- nvim/lua/custom/mappings.lua
+++ nvim/lua/custom/mappings.lua
@@ -37,4 +37,28 @@ M.general = {
   }
 }
 
+-- save if compiler is open
+local compiler_saved = false
+M.compiler = {
+--   -- Open compiler
+-- vim.api.nvim_set_keymap('n', '<F6>', "<cmd>CompilerOpen<cr>", { noremap = true, silent = true })
+--
+-- -- Redo last selected option
+-- vim.api.nvim_set_keymap('n', '<S-F6>',
+--      "<cmd>CompilerStop<cr>" -- (Optional, to dispose all tasks before redo)
+--   .. "<cmd>CompilerRedo<cr>",
+--  { noremap = true, silent = true })
+--
+-- -- Toggle compiler results
+-- vim.api.nvim_set_keymap('n', '<S-F7>', "<cmd>CompilerToggleResults<cr>", { noremap = true, silent = true })
+  plugin = true,
+  n = {
+    ["<F6>"] = { "<cmd>CompilerOpen<cr>", "CompilerOpen" },
+    ["<F7>"] = { "<cmd>CompilerToggleResults<cr>", "CompilerToggleResults" },
+    ["<F8>"] = { "<cmd>CompilerStop<cr>", "CompilerStop" },
+    ["<F9>"] = { "<cmd>CompilerRestart<cr>", "CompilerRestart" },
+  }
+}
+
+
 return M
