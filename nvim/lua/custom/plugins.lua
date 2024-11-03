a266107327a0ff0b96d727d5ca4c59537d1d0821
radekaadek <radekaadek@gmail.com>
2024-11-03 14:06:52 +0100

    cursor in neovim

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -3,6 +3,9 @@
 package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua"
 package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua"
 
+-- views can only be fully collapsed with the global statusline
+-- for avante.nvim
+vim.opt.laststatus = 3
 
 local plugins = {
   {
@@ -243,9 +246,6 @@ marksman = {},
     },
   },
   -- {
-  --   "nvim-java/nvim-java"
-  -- },
-  -- {
   --   "github/copilot.vim",
   --   lazy = false,
   --   config = function()  -- Mapping tab is already used by NvChad
@@ -423,8 +423,8 @@ marksman = {},
         sources = {
           null_ls.builtins.formatting.clang_format.with({
             extra_args = { "--style", "{BasedOnStyle: Chromium, ColumnLimit: 120}" },
-        }),
-      },
+          }),
+        },
       })
     end
   },
@@ -450,9 +450,9 @@ marksman = {},
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
-    ft = "markdown"
+    ---@type render.md.User  opts = { file_types = { "markdown", "Avante" } },
+    opts = { file_types = { "markdown", "Avante" } },
+    ft = { "markdown", "Avante" },
   },
   {
     'vhyrro/luarocks.nvim', -- for images
@@ -513,6 +513,54 @@ marksman = {},
     },
     opts = {}, -- your configuration
     ft = {"javascriptreact", "typescriptreact"}
+  },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false, -- set this if you want to always pull the latest change
+    opts = {
+      -- add any opts here
+      provider = "copilot",
+      hints = { enabled = false },
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
   }
 }
 return plugins
